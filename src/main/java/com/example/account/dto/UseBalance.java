a84e3c8f03afe9cb477e12f8260045482d482dd2
Shinbeom Kwon <faithbeom98@gmail.com>
2024-05-01 15:57:41 +0900

    잔액 사용 API 수정

---
+++ src/main/java/com/example/account/dto/UseBalance.java
@@ -0,0 +1,57 @@
+package com.example.account.dto;
+
+import com.example.account.type.TransactionResultType;
+import lombok.*;
+
+import javax.validation.constraints.*;
+import java.time.LocalDateTime;
+
+/**
+ * {
+ *     "userId":1,
+ *     "accountNumber":"1000000000",
+ *     "amount":1000
+ * }
+ */
+public class UseBalance {
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class Request {
+        @NotNull
+        @Min(1)
+        private Long userId;
+
+        @NotBlank
+        @Size(min = 10, max = 10)
+        private String accountNumber;
+
+        @NotNull
+        @Min(10)
+        @Max(100_000_000)
+        private Long amount;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Response {
+        private TransactionResultType transactionResultType;
+        private String transactionId;
+        private Long amount;
+        private String accountNumber;
+        private LocalDateTime transactedAt;
+
+        public static Response from(TransactionDto transactionDto) {
+            return Response.builder()
+                    .accountNumber(transactionDto.getAccountNumber())
+                    .transactionResultType(transactionDto.getTransactionResultType())
+                    .transactionId(transactionDto.getTransactionId())
+                    .amount(transactionDto.getAmount())
+                    .transactedAt(transactionDto.getTransactedAt())
+                    .build();
+        }
+    }
+}
